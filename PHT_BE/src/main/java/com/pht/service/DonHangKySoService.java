f441dc45220f9cada6fd84515009e782909e461c
DESKTOP-BEGCN1S\Thai Vu <thaivh19@gmail.com>
2025-09-15 04:55:39 +0700

    no message

---
+++ PHT_BE/src/main/java/com/pht/service/DonHangKySoService.java
@@ -0,0 +1,8 @@
+package com.pht.service;
+
+import com.pht.exception.BusinessException;
+
+public interface DonHangKySoService {
+    
+    String kySoDonHang(Long idDonHang, String serialNumber) throws BusinessException;
+}
